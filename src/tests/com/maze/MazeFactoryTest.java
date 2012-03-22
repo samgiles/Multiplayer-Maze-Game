2fcb568c97ee2c39bde9650cfb987b3cd432baf2
Samuel Giles <aphro.cag@googlemail.com>
2012-03-22 04:04:43 +0000

    Added test to MazeFactory test to test that a file was generated.

--- src/tests/com/maze/MazeFactoryTest.java
+++ src/tests/com/maze/MazeFactoryTest.java
@@ -37,14 +37,23 @@ public class MazeFactoryTest {
 	
 	@Test
 	public void testSimpleGridCreator() {
+		
+		// This simply doesn't do as expected due to something in WLV's implementation...
+		
 		IMazeGrid grid = MazeFactory.simpleGrid(mazeFileName, 200, 100);
-		assertTrue(mazeFileExists());
 		int x = grid.getSizeX();
 		int y = grid.getSizeY();
 		assertTrue("The X size of the grid was " + String.valueOf(x) + ", should have been 200", x == 200);
 		assertTrue("The Y size of the grid was " + String.valueOf(y) + ", should have been 100", y == 100);
 	}
 
+	@Test
+	public void testFileGeneration() {
+		IMazeGrid grid = MazeFactory.simpleGrid(mazeFileName, 10, 10);
+		assertTrue(mazeFileExists());
+		deleteMazeFile();
+	}
+	
 	private boolean mazeFileExists() {
 		File file = new File(mazeFileName);
 		return file.exists();
