6c548e9706abc700836d299e33e585c19895725e
Samuel Giles <aphro.cag@googlemail.com>
2012-03-23 07:24:42 +0000

    Started to code the maze class.

--- src/maze/Maze.java
+++ src/maze/Maze.java
@@ -1,5 +1,17 @@
 package maze;
 
+import com.maze.IMaze;
+
 public class Maze {
 
+	/**
+	 * Holds the underlying maze representation.
+	 */
+	private final IMaze maze;
+	
+	public Maze(IMaze maze) {
+		this.maze = maze;
+	}
+	
+	
 }
